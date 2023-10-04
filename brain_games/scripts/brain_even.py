7cd21eff07658b8b1ade7532a3aa99bcd29f1030
LaWorp <astvein@gmail.com>
2023-10-04 13:44:11 +0500

    Исправлена работа скриптов

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,9 +4,9 @@ import brain_games.parity_check
 
 def main():
     print('Welcome to the Brain Games!')
+    user_name = brain_games.cli.welcome_user()
+    brain_games.parity_check.game(user_name)
 
 
 if __name__ == '__main__':
     main()
-    user_name = brain_games.cli.welcome_user()
-    brain_games.parity_check.game(user_name)
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,8 +4,8 @@ import brain_games.cli
 
 def main():
     print('Welcome to the Brain Games!')
+    brain_games.cli.welcome_user()
 
 
 if __name__ == '__main__':
     main()
-    brain_games.cli.welcome_user()
