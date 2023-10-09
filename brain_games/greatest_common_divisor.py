1e89dd0221e51e037a8841b7ac634f89cd8ab560
LaWorp <astvein@gmail.com>
2023-10-09 16:17:45 +0500

    Добавление игры "НОД"

---
+++ brain_games/greatest_common_divisor.py
@@ -0,0 +1,39 @@
+from brain_games.parity_check import error_output
+from brain_games.calculator import gen_numbers
+
+
+def get_gcd(number_1, number_2):
+    min_number = get_min_number(number_1, number_2)
+    i = 1
+    while i <= min_number:
+        if number_1 % i == 0 and \
+           number_2 % i == 0:
+            gcd = i
+        i += 1
+    return gcd
+
+
+def get_min_number(number_1, number_2):
+    if number_1 < number_2:
+        min_number = number_1
+    else:
+        min_number = number_2
+    return min_number
+
+
+def game(name):
+    count = 0
+    print('Find the greatest common divisor of given numbers.')
+    while count < 3:
+        number_1, number_2 = gen_numbers()
+        gcd = get_gcd(number_1, number_2)
+        print(f'Question: {number_1} {number_2}')
+        answer = int(input('Your answer: '))
+        if answer == gcd:
+            print('Correct!')
+            count += 1
+        else:
+            error_output(answer, gcd, name)
+            break
+    if count == 3:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+import brain_games.cli
+import brain_games.greatest_common_divisor
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    user_name = brain_games.cli.welcome_user()
+    brain_games.greatest_common_divisor.game(user_name)
+
+
+if __name__ == '__main__':
+    main()
