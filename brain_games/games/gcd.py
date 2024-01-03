259ee259dcd7e2e81e48f566c0d9c6a7e507bdd8
RKV102 <roman.krilow@mail.ru>
2024-01-04 02:19:13 +0500

    Исправление пограничного случая

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,12 +7,14 @@ def give_question(_):
     return rand_value_1, rand_value_2
 
 
-def get_min_num(num_1, num_2):
+def get_min_max_num(num_1, num_2):
     if num_1 < num_2:
         min_num = num_1
+        max_num = num_2
     else:
         min_num = num_2
-    return min_num
+        max_num = num_1
+    return min_num, max_num
 
 
 def get_first_num(iterable):
@@ -26,9 +28,8 @@ def get_second_num(iterable):
 def det_answer(iterable):
     num_1 = get_first_num(iterable)
     num_2 = get_second_num(iterable)
-    min_num = get_min_num(num_1, num_2)
+    min_num, gcd = get_min_max_num(num_1, num_2)
     i = 1
-    gcd = 1
     while i <= min_num:
         if num_1 % i == 0 and \
            num_2 % i == 0:
