9b072efde6b9adf1e18da10b3febe58385008af0
RKV102 <roman.krilow@mail.ru>
2024-01-05 14:53:13 +0500

    Рефакторинг модуля game_init

--- brain_games/game_init.py
+++ brain_games/game_init.py
@@ -1,36 +1,17 @@
-from brain_games.game import game
-from brain_games.games.even import get_answer, check_answer
-from brain_games.games.even import give_question as give_question_even
-from brain_games.games.even import det_answer as det_answer_even
-from brain_games.games.calc import give_question as give_question_calc
-from brain_games.games.calc import det_answer as det_answer_calc
-from brain_games.games.gcd import give_question as give_question_gcd
-from brain_games.games.gcd import det_answer as det_answer_gcd
-from brain_games.games.progression import give_question as give_question_progr
-from brain_games.games.progression import det_answer as det_answer_progr
-from brain_games.games.prime import det_answer as det_answer_prime
+from brain_games.greeting import greeting
+from brain_games.qa import qa
 
 
 def game_init(game_module):
-    num_answer = False
-    progr_len = None
-    give_question = give_question_even
-    det_answer = det_answer_even
-    if game_name == 'calc':
-        num_answer = True
-        give_question = give_question_calc
-        det_answer = det_answer_calc
-    elif game_name == 'gcd':
-        num_answer = True
-        give_question = give_question_gcd
-        det_answer = det_answer_gcd
-    elif game_name == 'progr':
-        num_answer = True
-        progr_len = 10
-        give_question = give_question_progr
-        det_answer = det_answer_progr
-    else:
-        det_answer = det_answer_prime
-    game(num_answer, progr_len, give_question=give_question,
-         det_answer=det_answer, get_answer=get_answer,
-         check_answer=check_answer)
+    user_name = greeting()
+    descr = game_module.DESCR
+    num_answer = game_module.NUM_ANSWER
+    progr_len = game_module.PROGR_LEN
+    give_question = game_module.give_question
+    det_answer = game_module.det_answer
+    get_answer = game_module.get_answer
+    check_answer = game_module.check_answer
+    print(descr)
+    qa(user_name, num_answer, progr_len,
+       give_question=give_question, det_answer=det_answer,
+       get_answer=get_answer, check_answer=check_answer)
