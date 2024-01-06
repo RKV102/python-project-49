cc63affd236deed80c61f80efba794684b559477
RKV102 <roman.krilow@mail.ru>
2024-01-06 15:30:17 +0500

    Удаление всех вызовов check_answer()

--- brain_games/game_wrapper.py
+++ brain_games/game_wrapper.py
@@ -9,24 +9,21 @@ def launch_game(game):
     give_question = game.give_question
     det_answer = game.det_answer
     get_answer = game.get_answer
-    check_answer = game.check_answer
     print(description)
     qa(user_name, num_answer, progr_len,
        give_question=give_question, det_answer=det_answer,
-       get_answer=get_answer, check_answer=check_answer)
+       get_answer=get_answer)
 
 
 def qa(user_name, num_answer, progr_len, **kwargs):
     give_question = get_func('give_question', kwargs)
     det_answer = get_func('det_answer', kwargs)
     get_answer = get_func('get_answer', kwargs)
-    check_answer = get_func('check_answer', kwargs)
     count = 0
     while count < 3:
         question_hint = give_question(progr_len)
         right_answer = det_answer(question_hint)
         user_answer = get_answer(num_answer)
-        count = check_answer(right_answer, user_answer, count)
     if count != 4:
         print(f'Congratulations, {user_name}!')
     else:
