d2dda0dff929b588a5c8f751ff7a132bd69565a5
RKV102 <roman.krilow@mail.ru>
2024-01-06 15:05:29 +0500

    Перенос qa(), get_func() и greeting() в модуль game_init

--- brain_games/game_init.py
+++ brain_games/game_init.py
@@ -1,5 +1,4 @@
-from brain_games.greeting import greeting
-from brain_games.qa import qa
+from prompt import string
 
 
 def game_init(game_module):
@@ -15,3 +14,31 @@ def game_init(game_module):
     qa(user_name, num_answer, progr_len,
        give_question=give_question, det_answer=det_answer,
        get_answer=get_answer, check_answer=check_answer)
+
+
+def qa(user_name, num_answer, progr_len, **kwargs):
+    give_question = get_func('give_question', kwargs)
+    det_answer = get_func('det_answer', kwargs)
+    get_answer = get_func('get_answer', kwargs)
+    check_answer = get_func('check_answer', kwargs)
+    count = 0
+    while count < 3:
+        question_hint = give_question(progr_len)
+        right_answer = det_answer(question_hint)
+        user_answer = get_answer(num_answer)
+        count = check_answer(right_answer, user_answer, count)
+    if count != 4:
+        print(f'Congratulations, {user_name}!')
+    else:
+        print(f"Let's try again, {user_name}!")
+
+
+def get_func(func_name, func_set):
+    return func_set[func_name]
+
+
+def greeting():
+    print('Welcome to the Brain Games!')
+    user_name = string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    return user_name
--- brain_games/greeting.py
+++
@@ -1,8 +0,0 @@
-from prompt import string
-
-
-def greeting():
-    print('Welcome to the Brain Games!')
-    user_name = string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    return user_name
