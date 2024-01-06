1a9797ba63071b3bc6ab3e3491231c15c1a15eda
RKV102 <roman.krilow@mail.ru>
2024-01-06 15:11:03 +0500

    Изменение имени переменной descr

--- brain_games/game_init.py
+++ brain_games/game_init.py
@@ -3,14 +3,14 @@ from prompt import string
 
 def game_init(game_module):
     user_name = greeting()
-    descr = game_module.DESCRIPTION
+    description = game_module.DESCRIPTION
     num_answer = game_module.NUM_ANSWER
     progr_len = game_module.PROGR_LEN
     give_question = game_module.give_question
     det_answer = game_module.det_answer
     get_answer = game_module.get_answer
     check_answer = game_module.check_answer
-    print(descr)
+    print(description)
     qa(user_name, num_answer, progr_len,
        give_question=give_question, det_answer=det_answer,
        get_answer=get_answer, check_answer=check_answer)
