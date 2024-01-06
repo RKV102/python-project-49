b381c3d73c0e56ea6951b27ba7756f526dca0c50
RKV102 <roman.krilow@mail.ru>
2024-01-06 15:36:25 +0500

    Рефакторинг qa()

--- brain_games/game_wrapper.py
+++ brain_games/game_wrapper.py
@@ -20,15 +20,9 @@ def qa(user_name, num_answer, progr_len, **kwargs):
     give_question = get_func('give_question', kwargs)
     det_answer = get_func('det_answer', kwargs)
     get_answer = get_func('get_answer', kwargs)
-    count = 0
-    while count < 3:
-        question_hint = give_question(progr_len)
-        right_answer = det_answer(question_hint)
-        user_answer = get_answer(num_answer)
-    if count != 4:
-        print(f'Congratulations, {user_name}!')
-    else:
-        print(f"Let's try again, {user_name}!")
+    question_hint = give_question(progr_len)
+    right_answer = det_answer(question_hint)
+    user_answer = get_answer(num_answer)
     return user_answer
 
 
