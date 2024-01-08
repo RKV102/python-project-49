da1fce990a861cf3f79e2218a826e0738911cdad
RKV102 <roman.krilow@mail.ru>
2024-01-08 20:25:11 +0500

    Небольшой рефакторинг run_game()

--- brain_games/game_runner.py
+++ brain_games/game_runner.py
@@ -17,8 +17,8 @@ def run_game(game):
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
-                  + f"Correct answer was '{right_answer}'.")
-            print(f"Let's try again, {user_name}!")
+                  + f"Correct answer was '{right_answer}'.\n"
+                  + f"Let's try again, {user_name}!")
             break
     else:
         print(f'Congratulations, {user_name}!')
