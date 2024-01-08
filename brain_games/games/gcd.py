cd91283a2dfb92c0d0438ed5f4a8e061f4d4ba51
RKV102 <roman.krilow@mail.ru>
2024-01-08 21:01:14 +0500

    Рефакторинг generate_round()

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -11,13 +11,17 @@ def generate_round(start_question, _):
     match rand_num_1 < rand_num_2:
         case True:
             min_num = rand_num_1
+            max_num = rand_num_2
         case _:
             match rand_num_1 > rand_num_2:
                 case True:
                     min_num = rand_num_2
+                    max_num = rand_num_1
                 case _:
                     right_answer = str(rand_num_1)
                     return question, right_answer
+    if min_num == 0:
+        gcd = max_num
     for i in range(1, min_num + 1):
         if rand_num_1 % i == 0 and rand_num_2 % i == 0:
             gcd = i
