4e3372e9895c4d1c98a95dba21fd47c1963ea0e0
RKV102 <roman.krilow@mail.ru>
2024-01-09 15:05:48 +0500

    Процесс поиска НОД вынесен в отдельную функцию

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,6 +8,12 @@ def generate_round(_):
     rand_num_1 = randint(0, 100)
     rand_num_2 = randint(0, 100)
     question = f'{rand_num_1} {rand_num_2}'
+    gcd = find_gcd(rand_num_1, rand_num_2)
+    right_answer = str(gcd)
+    return question, right_answer
+
+
+def find_gcd(rand_num_1, rand_num_2):
     match rand_num_1 < rand_num_2:
         case True:
             min_num = rand_num_1
@@ -20,8 +26,7 @@ def generate_round(_):
     for i in range(1, min_num + 1):
         if rand_num_1 % i == 0 and rand_num_2 % i == 0:
             gcd = i
-    right_answer = str(gcd)
-    return question, right_answer
+    return gcd
 
 
 predicate = None
