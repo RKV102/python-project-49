f46b17f7855de996a91d53485546fb143c7bef0e
RKV102 <roman.krilow@mail.ru>
2024-01-10 17:02:47 +0500

    Удалена hide_item_in_progr(). Её содержимое перенесено в generate_round()

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -13,16 +13,9 @@ def create_progression():
     return list(range(number, number + step * PROGRESSION_LEN, step))
 
 
-def hide_item_in_progression(progression):
-    hidden_pos = choice(range(PROGRESSION_LEN))
-    hidden_item = str(progression[hidden_pos])
-    progression[hidden_pos] = '..'
-    return hidden_item, progression
-
-
-def create_question(new_progression):
+def create_question(progression):
     question = ''
-    for pos, item in enumerate(new_progression):
+    for pos, item in enumerate(progression):
         if pos == 0:
             question += str(item)
         else:
@@ -32,7 +25,9 @@ def create_question(new_progression):
 
 def generate_round():
     progression = create_progression()
-    hidden_item, new_progression = hide_item_in_progression(progression)
-    question = create_question(new_progression)
+    hidden_pos = choice(range(PROGRESSION_LEN))
+    hidden_item = str(progression[hidden_pos])
+    progression[hidden_pos] = '..'
+    question = create_question(progression)
     right_answer = hidden_item
     return question, right_answer
